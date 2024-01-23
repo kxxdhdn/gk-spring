9e2b154c03ca1baf8e2b66310f948a6484063e71
kxxdhdn <zayintopology@gmail.com>
2024-01-23 12:48:06 +0100

    Exo : ajout CompteController [fait]

--- gk_ams/src/main/java/com/sip/ams/controllers/BanqueController.java
+++ gk_ams/src/main/java/com/sip/ams/controllers/BanqueController.java
@@ -5,13 +5,25 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.sip.ams.entities.Banque;
 
 @Controller // Annotation pour rendre la classe comme un controlleur
 public class BanqueController { // un controlleur
+
+	static List<Banque> listeb = new ArrayList<>();
+
+	// bloc static s'exécute lors de l'invocation de la classe BanqueController
+	static {
+		Banque b1 = new Banque("SG", "Lyon, France", 200000);
+		Banque b2 = new Banque("LCL", "Nantes, France", 100000);
+		Banque b3 = new Banque("BNP", "Paris, France", 300000);
+
+		listeb.add(b1);
+		listeb.add(b2);
+		listeb.add(b3);
+	}
 
 	@RequestMapping("/hello")
 	@ResponseBody // retourner le résultat entre les "" sur le navigateur
@@ -51,18 +63,39 @@ public class BanqueController { // un controlleur
 
 	@RequestMapping("/banques")
 	public String listeBanques(Model model) {
-		List<Banque> listeb = new ArrayList<>();
-
-		Banque b1 = new Banque("SG", "Lyon, France", 200000);
-		Banque b2 = new Banque("LCL", "Nantes, France", 100000);
-		Banque b3 = new Banque("BNP", "Paris, France", 300000);
-
-		listeb.add(b1);
-		listeb.add(b2);
-		listeb.add(b3);
-
 		model.addAttribute("banques", listeb);
 		return "banque/banques"; // chemin vers le fichier html
+	}
+
+	@GetMapping("/addBanque")
+	public String formBanque() {
+		return "banque/formBanque.html";
+	}
+
+	@PostMapping("/addBanque") // pour les formulaires en mode post
+//	@ResponseBody
+	public String saveBanque(@RequestParam("nomBanque") String nomB, @RequestParam("adresseBanque") String adrB,
+			@RequestParam("capitalBanque") double capB) {
+
+//		 return "banque/formBanque.html";
+//		return nomB + " - " + adrB + " - " + capB;
+		Banque banque = new Banque(nomB, adrB, capB);
+		listeb.add(banque);
+		// return banque.toString();
+		return "redirect:banques"; // ici on met la route de l'action pour faire une redirection
+	}
+
+	@GetMapping("/deleteBanque/{nomBanque}")
+	// @ResponseBody
+	public String deleteBanque(@PathVariable("nomBanque") String nomBanque) {
+		Banque targetBanque = null;
+		for (Banque banque : listeb) {
+			if (banque.getNomBanque().equals(nomBanque)) {
+				targetBanque = banque;
+			}
+		}
+		listeb.remove(targetBanque);
+		return "redirect:../banques";
 	}
 
 }
--- gk_ams/src/main/java/com/sip/ams/controllers/CompteController.java
+++ gk_ams/src/main/java/com/sip/ams/controllers/CompteController.java
@@ -6,17 +6,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.sip.ams.entities.CompteBancaire;
 
 @Controller
 public class CompteController {
 
-	@RequestMapping("/comptes")
-	public String listeComptes(Model model) {
-		List<CompteBancaire> listecb = new ArrayList<>();
+	static List<CompteBancaire> listecb = new ArrayList<>();
 
+	static {
 		CompteBancaire cb1 = new CompteBancaire(1, "Einstein Albert", 12345, LocalDate.of(2024, 1, 10), 111);
 		CompteBancaire cb2 = new CompteBancaire(2, "Feynman Richard", 54321, LocalDate.of(2024, 1, 18), 111);
 		CompteBancaire cb3 = new CompteBancaire(3, "Bohr Niels", 10000, LocalDate.of(2024, 1, 23), 111);
@@ -24,9 +23,42 @@ public class CompteController {
 		listecb.add(cb1);
 		listecb.add(cb2);
 		listecb.add(cb3);
+	}
 
+	@RequestMapping("/comptes")
+	public String listeComptes(Model model) {
 		model.addAttribute("comptes", listecb);
 		return "banque/comptes"; // chemin vers le fichier html
+	}
+
+	@GetMapping("/addCompte")
+	public String formCompte() {
+		return "banque/formCompte.html";
+	}
+
+	@PostMapping("/addCompte") // pour les formulaires en mode post
+//	@ResponseBody
+	public String saveCompte(@RequestParam("numCompte") int numCB, @RequestParam("proprietaire") String ppt,
+			@RequestParam("solde") double sol, @RequestParam("dateCreation") LocalDate datCr,
+			@RequestParam("idBanque") int idB) {
+
+		CompteBancaire cb = new CompteBancaire(numCB, ppt, sol, datCr, idB);
+		listecb.add(cb);
+		// return banque.toString();
+		return "redirect:comptes"; // ici on met la route de l'action pour faire une redirection
+	}
+
+	@GetMapping("/deleteCompte/{numCompte}")
+	// @ResponseBody
+	public String deleteCompte(@PathVariable("numCompte") int numCompte) {
+		CompteBancaire targetCompte = null;
+		for (CompteBancaire cb : listecb) {
+			if (cb.getNumCompte() == numCompte) {
+				targetCompte = cb;
+			}
+		}
+		listecb.remove(targetCompte);
+		return "redirect:../comptes";
 	}
 
 }
